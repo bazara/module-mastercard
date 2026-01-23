b6cfb1370655116f8db44ff64dc02d3d6392054c
Ivan Bila <ivan.bila@vm.co.mz>
2026-01-23 15:15:05 +0200

    deprecated zend framework

--- Gateway/Http/Client/Adapter/Rest.php
+++ Gateway/Http/Client/Adapter/Rest.php
@@ -18,9 +18,9 @@
 namespace OnTap\MasterCard\Gateway\Http\Client\Adapter;
 
 use Magento\Framework\HTTP\Adapter\Curl;
-use Zend_Http_Client;
-use Zend_Uri_Exception;
-use Zend_Uri_Http;
+use Laminas\Http\Client;
+use Laminas\Uri\Exception\ExceptionInterface as UriException;
+use Laminas\Uri\Http;
 
 class Rest extends Curl
 {
@@ -28,16 +28,16 @@ class Rest extends Curl
      * Send request to the remote server
      *
      * @param string $method
-     * @param Zend_Uri_Http|string $url
+     * @param Http|string $url
      * @param string $httpVer
      * @param array $headers
      * @param string $body
      * @return string Request as text
-     * @throws Zend_Uri_Exception
+     * @throws UriException
      */
     public function write($method, $url, $httpVer = '1.1', $headers = [], $body = '')
     {
-        if ($url instanceof Zend_Uri_Http) {
+        if ($url instanceof Http) {
             $url = $url->getUri();
         }
         $this->_applyConfig();
@@ -47,15 +47,15 @@ class Rest extends Curl
         curl_setopt($this->_getResource(), CURLOPT_URL, $url);
         curl_setopt($this->_getResource(), CURLOPT_RETURNTRANSFER, true);
 
-        if ($method == Zend_Http_Client::POST) {
+        if ($method == Client::POST) {
             curl_setopt($this->_getResource(), CURLOPT_POST, true);
             curl_setopt($this->_getResource(), CURLOPT_POSTFIELDS, $body);
             $headers[] = 'Content-Length: ' . strlen($body);
-        } elseif ($method == Zend_Http_Client::PUT) {
-            curl_setopt($this->_getResource(), CURLOPT_CUSTOMREQUEST, Zend_Http_Client::PUT);
+        } elseif ($method == Client::PUT) {
+            curl_setopt($this->_getResource(), CURLOPT_CUSTOMREQUEST, Client::PUT);
             curl_setopt($this->_getResource(), CURLOPT_POSTFIELDS, $body);
             $headers[] = 'Content-Length: ' . strlen($body);
-        } elseif ($method == Zend_Http_Client::GET) {
+        } elseif ($method == Client::GET) {
             curl_setopt($this->_getResource(), CURLOPT_HTTPGET, true);
         }
 
